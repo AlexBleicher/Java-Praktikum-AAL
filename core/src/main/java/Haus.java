dc25cea3f0cb2b362fadbb055498c6164f989534
AlexBleicher <alexander.bleicher@gmail.com>
2021-02-15 11:45:44 +0100

    Anfang Vorbereitung des Spielfelds (Hinzugefügt: Haus und Figur)
    #8 BackendStartseite

---
+++ core/src/main/java/Haus.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Haus {
+
+    private Spieler spieler;
+    private Feld startFeld;
+    private List<Figur> enthalteneFiguren = new ArrayList<>();
+}
--- core/src/main/java/Spielbrett.java
+++ core/src/main/java/Spielbrett.java
@@ -4,11 +4,12 @@ import java.util.List;
 public class Spielbrett {
 
     private List<Feld> felder = new ArrayList<>();
-
+    private List<Haus> haeuser = new ArrayList<>();
     public void bereiteFeld() {
         for (int i = 0; i < 40; i++) {
             Feld currentField = new Feld(i + 1);
             felder.add(currentField);
         }
     }
+
 }
