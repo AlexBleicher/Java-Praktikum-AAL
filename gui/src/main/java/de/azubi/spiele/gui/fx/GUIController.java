4db3367e0ecce40d76520a49671a3543c02e5754
Leon Wolf <leon.wolf.leon@hotmail.com>
2021-02-15 15:42:45 +0100

    implemented functions // eintragen + starten

--- gui/src/main/java/de/azubi/spiele/gui/fx/GUIController.java
+++ gui/src/main/java/de/azubi/spiele/gui/fx/GUIController.java
@@ -1,9 +1,13 @@
 package de.azubi.spiele.gui.fx;
 
+import de.aal.spiel.core.LogikStart;
+import de.aal.spiel.core.SpielManager;
+import de.aal.spiel.core.Spiellogik;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.apache.commons.logging.Log;
 
 public class GUIController {
 
@@ -13,13 +17,17 @@ public class GUIController {
     @FXML
     private TextField tfName;
 
+    private SpielManager spielManager = new SpielManager();
+    private Spiellogik spiellogik = new Spiellogik();
+    private LogikStart logikStart = new LogikStart(spiellogik, spielManager);
+
     public void eintragen(ActionEvent actionEvent) {
         String name = tfName.getText();
-        // Eintragung in LogikSpieler
+        logikStart.spielerErstellen(name);
         taSpieler.setText(taSpieler.getText() + name + "\n");
     }
 
     public void starten(ActionEvent actionEvent) {
-        // Starten des Spiels
+        logikStart.spielStarten();
     }
 }
