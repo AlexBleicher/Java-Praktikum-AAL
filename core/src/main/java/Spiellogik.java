0ff9055331a6b31d5df23b1436a75d4c6c473766
AlexBleicher <73163576+AlexBleicher@users.noreply.github.com>
2021-02-15 10:54:36 +0100

    Update Spiellogik.java

--- core/src/main/java/Spiellogik.java
+++ core/src/main/java/Spiellogik.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Spiellogik {
-
-
+    
     private List<Spieler> spielerList=new ArrayList<>();
 
     public void addSpieler(Spieler p){
